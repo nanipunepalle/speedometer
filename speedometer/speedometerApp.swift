84a373a05dc40c49dfb2ca42ca8c0d667ada2a53
Lalith <lalithpunepalli@gmail.com>
2021-06-25 17:29:49 +0530

    Initial Commit

---
+++ speedometer/speedometerApp.swift
@@ -0,0 +1,17 @@
+//
+//  speedometerApp.swift
+//  speedometer
+//
+//  Created by Lalith  on 25/06/21.
+//
+
+import SwiftUI
+
+@main
+struct speedometerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
