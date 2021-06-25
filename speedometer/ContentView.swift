88243f813a460c6a88709da0fe4bff488359f099
Lalith <lalithpunepalli@gmail.com>
2021-06-25 21:46:10 +0530

    basic app completed

--- speedometer/ContentView.swift
+++ speedometer/ContentView.swift
@@ -8,14 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @ObservedObject var locationController: LocationController
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("SpeedoMeter")
+                .padding()
+            Spacer()
+            
+//            Spacer()
+            ZStack {
+                ZStack{
+                    Circle()
+                        .trim(from: 0, to: 0.8)
+                        .stroke(Color.primary.opacity(0.1),lineWidth: 55)
+                        .frame(width: 300, height: 300, alignment: .center)
+                    Circle()
+                        .trim(from: 0, to: CGFloat(Int(locationController.speed*3.6)/200))
+                        .stroke(Color.green.opacity(0.5),lineWidth: 55)
+                        .frame(width: 300, height: 300, alignment: .center)
+                        
+                }.rotationEffect(.init(degrees: 128))
+                ZStack(alignment:.bottom){
+                    VStack {
+                        Text(String(Int(locationController.speed*3.6)))
+//                            .foregroundColor(Color.black.opacity(1))
+                            .font(Font.system(size:130, design: .default))
+                        Text("KMPH")
+//                            .foregroundColor(Color.black.opacity(1))
+                            .font(Font.system(size:30, design: .default))
+                    }
+                    
+//                    Rectangle()
+//                        .fill(Color.red)
+//                        .frame(width: 3, height: 105)
+                }
+//                .offset(y:-35)
+//                .rotationEffect(.init(degrees: -135))
+            }
+            Spacer()
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(locationController: LocationController())
     }
 }
---
+++ speedometer/LocationController.swift
@@ -0,0 +1,72 @@
+//
+//  LocationController.swift
+//  speedometer
+//
+//  Created by Lalith  on 25/06/21.
+//
+
+import Foundation
+import CoreLocation
+import SwiftUI
+import Combine
+
+class LocationController: NSObject, ObservableObject, CLLocationManagerDelegate {
+    var locationManager: CLLocationManager!
+    let objectWillChange = PassthroughSubject<LocationController, Never>()
+
+    
+    var speed: Double = 0 {
+        didSet {
+            objectWillChange.send(self)
+        }
+    }
+    
+    override init() {
+        super.init()
+        setupLocationService()
+    }
+    
+    
+    func setupLocationService() {
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.requestAlwaysAuthorization()
+        locationManager.desiredAccuracy=kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+    }
+    
+    func getLocation(){
+        print(locationManager.requestLocation())
+    }
+    
+    private func locationManager(manager: CLLocationManager!, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        print("a")
+        if status != CLAuthorizationStatus.denied{
+            locationManager.startUpdatingLocation()
+        }
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+//        if status != CLAuthorizationStatus.denied{
+            locationManager.startUpdatingLocation()
+//        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if(locationManager.location!.speed > 0){
+            speed = locationManager.location!.speed
+        }
+        else{
+            speed = 0
+        }
+        
+//        print(locationManager.location!.speedAccuracy)
+//        print(speed)
+        objectWillChange.send(self)
+    }
+    
+    func locationManagerDidResumeLocationUpdates(_ manager: CLLocationManager) {
+
+    }
+    
+}
